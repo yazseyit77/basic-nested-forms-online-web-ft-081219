8cbe86dfa2e66312d480b74ca5e39ff8cbfb3e22
yazseyit77 <yazgeldi.seyidov@gmail.com>
2019-10-27 11:04:04 -0700

    Done.

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,3 +1,9 @@
 class Address < ActiveRecord::Base
   belongs_to :person
+  
+  def person_attributes=(person)
+    self.person = Person.find_or_create_by(name: person[:name])
+    self.person.update(person)
+  end
+
 end
--- app/models/person.rb
+++ app/models/person.rb
@@ -1,4 +1,10 @@
 class Person < ActiveRecord::Base
   has_many :addresses
+  accepts_nested_attributes_for :addresses
+
+  def address_attributes=(address)
+    self.address = Address.find_or_create_by(name: address[:name])
+    self.address.update(artist)
+  end
 
 end
